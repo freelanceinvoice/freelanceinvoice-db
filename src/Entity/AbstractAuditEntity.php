2e02f6b4781fbff953175e6ac39537a5c94aab43
jpg-consulting <opensource@jpg-consulting.es>
2017-12-23 16:27:40 +0100

    first commit

---
+++ src/Entity/AbstractAuditEntity.php
@@ -0,0 +1,71 @@
+<?php
+namespace FreelanceInvoice\Db\Entity;
+
+use DateTime;
+
+abstract class AbstractAuditEntity
+{
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $createdBy;
+
+    /**
+     * @Column(type="datetime")
+     * @var DateTime
+     */
+    protected $createdAt;
+    
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $updatedBy;
+    
+    /**
+     * @Column(type="datetime")
+     * @var DateTime
+     */
+    protected $updatedAt;
+
+    /**
+     * Gets the date when entry was created.
+     * 
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+    
+    /**
+     * Gets the user name that created the entry.
+     * 
+     * @return string
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+    
+    /**
+     * Gets the date when entry was updated.
+     *
+     * @return DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+    
+    /**
+     * Gets the user name that updated the entry.
+     * 
+     * @return string
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+}
